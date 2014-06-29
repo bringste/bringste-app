1cb1a8870c35f76f037ead9e68e7d421869fa510
Nico Rehwaldt <git_nikku@nixis.de>
2014-06-30 00:15:18 +0200

    test(env): add environment spec

---
+++ test/spec/environmentSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var angular = require('angular');
+
+
+describe('environment', function() {
+
+  it('should provide angular', function() {
+    expect(angular).toBeDefined();
+  });
+
+});
